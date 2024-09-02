63d7a174070281029159371cdff601897f22db19
Hector-Junior <nuagecloud43@gmail.com>
2024-09-02 12:21:41 +0200

    add animation

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './component/Card.js';
 import Card from './component/Card.js';
 import { useState } from 'react';
 
-// Génére une carte random avec un nombre de fruits et un type (pomme, pain ou pomme de pin) 1 à 3 fruits/carte
 function randomCard() {
   let card = [];
   let randomNumber = Math.floor(Math.random() * 3) + 1;
@@ -16,20 +15,26 @@ function randomCard() {
 }
 
 export default function App() {
+  
   const [items, setItems] = useState([
     { id: 1, value: [] },
     { id: 2, value: [] },
     { id: 3, value: [] },
   ]);
-  const [counter, setCounter] = useState(0);
-
+  const [counter, setCounter] = useState(0); 
   const regenerateCard = (id) => {
-    setItems((prevItems) =>
-      prevItems.map((item) =>
-        item.id === id ? { ...item, value: randomCard() } : item
-      )
-    );
-    setCounter(counter + 1);
+    const interval = setInterval(() => {
+      setItems((prevItems) =>
+        prevItems.map((item) =>
+          item.id === id ? { ...item, value: randomCard() } : item
+        )
+      );
+    }, 100); 
+
+    setTimeout(() => {
+      clearInterval(interval);
+      setCounter(counter + 1); 
+    }, 1000); 
   };
 
   const clearAllCards = () => {
@@ -42,18 +47,16 @@ export default function App() {
   return (
     <div className="App">
       <h1>🍎🥖🥖</h1>
-      <div className='content'>
-        <div className="listCard">
-          {items.map((item) => (
-            <div key={item.id}>
-              <Card values={item.value} />
-              <button className='bouton' onClick={() => regenerateCard(item.id)}>Regenerate</button>
-            </div>
-          ))}
-        </div>
-        <h2 className='counter'>Counter: {counter}</h2> {/* Affichage du compteur */}
-        <button className='bouton-clear' onClick={clearAllCards}>Clear All</button>
+      <div className="listCard">
+        {items.map((item) => (
+          <div key={item.id}>
+            <Card values={item.value} />
+            <button className='bouton' onClick={() => regenerateCard(item.id)}>Regenerate</button>
+          </div>
+        ))}
       </div>
+      <h2>Regenerate Count: {counter}</h2>
+      <button className='bouton-clear' onClick={clearAllCards}>Clear All</button>
     </div>
   );
 }
